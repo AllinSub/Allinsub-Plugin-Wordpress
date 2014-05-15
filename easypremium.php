de8e75e1facff45913fd3519dfb0bc58343750ef
Julien Del Rio <jdelrio@levelupstudio.com>
2014-05-15 19:18:43 +0200

    Populate "Je m'abonne" dialog Change content on EasyPremium Button

--- easypremium.php
+++ easypremium.php
@@ -8,29 +8,29 @@ Description: Plugin ready to use for EasyPremium.
 */
 
 
-class EP_Plugin
-{
-    public function __construct()
-    {
+class EP_Plugin {
+	public function __construct() {
 		// Load scripts
-		add_action( 'init', 'load_scripts' );
+		add_action ( 'init', 'load_scripts' );
 		
 		// Include Admin components
-        include_once plugin_dir_path( __FILE__ ).'/admin.php';
-        new EP_Admin_Panel();
+		include_once plugin_dir_path ( __FILE__ ) . '/admin.php';
+		new EP_Admin_Panel ();
 		
 		// Include widgets components
-        include_once plugin_dir_path( __FILE__ ).'/widgets.php';
-		new EP_Widgets_Manager();
-    }
+		include_once plugin_dir_path ( __FILE__ ) . '/widgets.php';
+		new EP_Widgets_Manager ();
+	}
 }
 
-new EP_Plugin();
+new EP_Plugin ();
 
 function load_scripts() {
-    wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'jquery-ui-dialog', array( 'jquery' ) );
-	wp_register_style('jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css');
-	wp_enqueue_style( 'jquery-ui' ); 
+	wp_enqueue_script ( 'jquery' );
+	wp_enqueue_script ( 'jquery-ui-dialog', array (
+			'jquery' 
+	) );
+	wp_register_style ( 'jquery-ui', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css' );
+	wp_enqueue_style ( 'jquery-ui' );
 }
 ?>
--- js/easypremium-core.js
+++ js/easypremium-core.js
@@ -1,5 +1,27 @@
 function showAuthDialog()
 {
 	// window.alert("Test");
-    jQuery( "#dialog" ).dialog({ width: 600, modal: true, resizable: false });
+	var dialog = jQuery( "#dialog" );
+	dialog.html("" +
+			'<div class="EPframe">' +
+			'	<div class="EPframeTitle">Je crée mon compte</div>' +
+			'	<div class="buttonsDiv">' +
+			'		<button id="loginFacebook" onClick="loginFacebook()">Facebook</button><button id="loginEasyPremium" onClick="loginEasyPremium()">EasyPremium</button>' +
+			'	</div>' +
+			'</div>' +
+			'<button id="createEasyPremium" onClick="createEasyPremium()">J\'ai déjà un compte</button>');
+	dialog.dialog({ title: "", modal: true, resizable: false });
+}
+
+function loginEasyPremium()
+{
+	// window.alert("Test");
+	var dialog = jQuery( "#dialog" );
+	dialog.html("" +
+			'<div class="EPframe">' +
+			'	<div class="EPframeTitle">Nouvel utilisateur</div>' +
+			'	<div class="buttonsDiv">' +
+			'	</div>' +
+			'</div>' +
+			'');
 }
